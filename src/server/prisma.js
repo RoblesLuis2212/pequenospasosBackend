3a2f9c49ce3714e7ffaaa9ba7c078502add98c6f
Luis Robles <luisgeremiasrobles@gmail.com>
2026-04-01 19:52:58 -0300

    Complete el schema de la base de datos con las tablas correspondientes

--- src/server/prisma.js
+++ src/server/prisma.js
@@ -1,7 +1,9 @@
 import { prismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
+//Guardo la cadena de conexion con mi motor de PostgreSQL
 const cadenaConexion = `${process.env.DATABASE_URL}`;
 
 const adapter = new prismaPg({ cadenaConexion });
+//Creo un cliente prisma para poder interactuar con la base de datos
 export const prisma = new PrismaClient({ adapter });
