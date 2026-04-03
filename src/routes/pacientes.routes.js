18ac254e7526eb7bf74877953a75b26091350543
Luis Robles <luisgeremiasrobles@gmail.com>
2026-04-03 19:49:42 -0300

    Agregue un controlador para poder listar informacion del paciente, agregarlos y editar sus datos personales

--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { agregarPaciente } from "../controllers/pacientes.controllers.js";
+import {
+  actualizarDatos,
+  agregarPaciente,
+  listarPacientes,
+  obtenerPacienteID,
+} from "../controllers/pacientes.controllers.js";
 
 const router = Router();
-router.route("/").post(agregarPaciente);
+router.route("/").post(agregarPaciente).get(listarPacientes);
+router.route("/:id").put(actualizarDatos).get(obtenerPacienteID);
 export default router;
