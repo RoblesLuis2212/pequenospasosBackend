e6bb6fcb21425b2a539011d236d45e376a25cf08
Luis Robles <luisgeremiasrobles@gmail.com>
2026-04-16 18:25:12 -0300

    agregue dos controladores uno para ver finalizar la compra del usuario y otro para poder listar sus compras pendientes de aprobacion

--- src/controllers/carrito.controllers.js
+++ src/controllers/carrito.controllers.js
@@ -115,3 +115,63 @@ export const eliminarProductoCarrito = async (req, res) => {
     });
   }
 };
+
+//Este controlador finalizara la compra del usuario, con un estado pendiente, que sera aprobado por el administrador
+export const finalizarCompra = async (req, res) => {
+  try {
+    //Se extrae el id del usuario del token para establecer la relacion
+    const usuarioId = req.usuario.idUsuario;
+
+    const carritoBuscado = await prisma.carrito.findUnique({
+      where: { idCarrito: Number(req.params.id), usuarioId: usuarioId },
+    });
+
+    if (!carritoBuscado) {
+      return res.status(404).json({ mensaje: "Carrito no encontrado" });
+    }
+
+    const nuevoEstado =
+      carritoBuscado.estado === "ACTIVO" ? "PENDIENTE" : carritoBuscado.estado;
+
+    const carritoActualizado = await prisma.carrito.update({
+      where: { idCarrito: Number(req.params.id) },
+      data: { estado: nuevoEstado },
+    });
+
+    res.status(200).json({ mensaje: "Compra finalizada correctamente" });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al finalizar la compra" });
+  }
+};
+
+export const listarComprasUsuario = async () => {
+  try {
+    const usuarioId = req.usuario.idUsuario;
+
+    const compras = await prisma.carrito.findMany({
+      where: { usuarioId, estado: "PENDIENTE" },
+      include: {
+        detalleCarritos: {
+          include: {
+            producto: {
+              select: {
+                idProducto: true,
+                nombre: true,
+                precio: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    res.status(200).json({ compras });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al obtener las compras del usuario" });
+  }
+};
--- src/routes/carrito.routes.js
+++ src/routes/carrito.routes.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 import {
   agregarAlCarrito,
   eliminarProductoCarrito,
+  finalizarCompra,
   listarCarritoUsuario,
+  listarComprasUsuario,
 } from "../controllers/carrito.controllers.js";
 import verificarToken from "../middlewares/validarToken.js";
 import validacionID from "../middlewares/validacionID.js";
@@ -12,8 +14,10 @@ router
   .route("/")
   .post(verificarToken, agregarAlCarrito)
   .get(verificarToken, listarCarritoUsuario);
+router.route("/compras").get(verificarToken, listarComprasUsuario);
 router
   .route("/:id")
-  .delete(verificarToken, validacionID, eliminarProductoCarrito);
+  .delete(verificarToken, validacionID, eliminarProductoCarrito)
+  .patch(verificarToken, validacionID, finalizarCompra);
 
 export default router;
