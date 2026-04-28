b916b9b6bed30c60bb2ca657757fc990d732caeb
Luis Robles <luisgeremiasrobles@gmail.com>
2026-04-28 15:44:36 -0300

    agregue un controlador para poder registrar los turnos abonado

--- src/controllers/caja.controllers.js
+++ src/controllers/caja.controllers.js
@@ -25,3 +25,82 @@ export const abrirCaja = async (req, res) => {
     res.status(500).json({ mensaje: "Ocurrio un error al abrir la caja" });
   }
 };
+
+export const regitrarPagoTurno = async (req, res) => {
+  try {
+    const usuarioId = req.usuario.idUsuario;
+
+    const cajaAbierta = await prisma.caja.findFirst({
+      where: { usuarioId: usuarioId, estado: "ABIERTA" },
+    });
+
+    if (!cajaAbierta) {
+      return res.status(400).json({ mensaje: "No hay una caja abierta" });
+    }
+
+    const turno = await prisma.turno.findUnique({
+      where: { idTurno: Number(req.params.id) },
+      include: {
+        paciente: {
+          select: {
+            nombreCompleto: true,
+            dni: true,
+            obraSocial: true,
+          },
+        },
+      },
+    });
+
+    if (!turno) {
+      return res.status(404).json({ mensaje: "Turno no encontrado" });
+    }
+
+    const precioConsulta = turno.paciente.obraSocial?.precioConsulta || 25000;
+
+    const ventaExistente = await prisma.ventas.findFirst({
+      where: { turnoId: Number(req.params.id) },
+    });
+
+    if (ventaExistente) {
+      return res.status(400).json({ mensaje: "El turno ya fue pagado" });
+    }
+
+    const nuevaVenta = await prisma.ventas.create({
+      data: {
+        monto: precioConsulta,
+        tipoVenta: "CONSULTA",
+        estado: "APROBADO",
+        usuarioId,
+        turnoId: Number(req.params.id),
+        cajaId: cajaAbierta.idCaja,
+      },
+    });
+
+    const turnoActualizado = await prisma.turno.update({
+      where: { idTurno: Number(req.params.id) },
+      data: { estado: "FINALIZADO" },
+    });
+
+    res
+      .status(200)
+      .json({ mensaje: "Pago del turno registrado correctamente" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ mensaje: "Ocurrio un error al registrar la venta" });
+  }
+};
+
+export const listarVentas = async (req, res) => {
+  try {
+    const ventas = await prisma.ventas.findMany();
+
+    if (!ventas) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontraron ventas para listar" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ mensaje: "Ocurrio un error al listar las ventas" });
+  }
+};
--- src/routes/caja.routes.js
+++ src/routes/caja.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { abrirCaja } from "../controllers/caja.controllers.js";
+import {
+  abrirCaja,
+  listarVentas,
+  regitrarPagoTurno,
+} from "../controllers/caja.controllers.js";
 import verificarToken from "../middlewares/validarToken.js";
 
 const router = Router();
-router.route("/").post(verificarToken, abrirCaja);
+router
+  .route("/")
+  .post(verificarToken, abrirCaja)
+  .get(verificarToken, listarVentas);
+router.route("/:id/turno-pago").put(verificarToken, regitrarPagoTurno);
 
 export default router;
