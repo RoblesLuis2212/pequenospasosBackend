434cbea97255f810f83ffc7da204ec13ede90ea8
Luis Robles <luisgeremiasrobles@gmail.com>
2026-05-23 20:06:20 -0300

    agregue funciones al controlador de evolucion del paciente para registrar una evolucion y listarlas

--- src/controllers/evolucionPaciente.controllers.js
+++ src/controllers/evolucionPaciente.controllers.js
@@ -6,12 +6,49 @@ export const agregarEvolucion = async (req, res) => {
     const { id } = req.params;
 
     const paciente = await prisma.paciente.findUnique({
-      // where: {pacienteId: }
+      where: { idPaciente: Number(id) },
     });
+
+    if (!paciente) {
+      return res.status(404).json({ mensaje: "Paciente no encontrado" });
+    }
+
+    const evolucion = await prisma.evolucionPaciente.create({
+      data: {
+        descripcion,
+        fecha: new Date(),
+        pacienteId: Number(id),
+      },
+    });
+
+    res
+      .status(201)
+      .json({ mensaje: "Evolucion del paciente registrada correctamente" });
   } catch (err) {
     console.error(err);
     res.status(500).json({
       mensaje: "Ocurrio un error al agregar la evolucion del paciente",
+    });
+  }
+};
+
+export const listarEvolucionPaciente = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const evolucionPaciente = await prisma.evolucionPaciente.findMany({
+      where: { pacienteId: Number(id) },
+    });
+
+    if (!evolucionPaciente) {
+      return res
+        .status(404)
+        .json("No hay registros de la evolucion del paciente");
+    }
+    res.status(200).json(evolucionPaciente);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      mensaje: "Ocurrio un error al listar la evolucion del paciente",
     });
   }
 };
--- src/routes/evolucionPaciente.routes.js
+++ src/routes/evolucionPaciente.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import {
+  agregarEvolucion,
+  listarEvolucionPaciente,
+} from "../controllers/evolucionPaciente.controllers.js";
 
 const router = Router();
-router.route("/:id");
+router.route("/:id").post(agregarEvolucion).get(listarEvolucionPaciente);
 
 export default router;
